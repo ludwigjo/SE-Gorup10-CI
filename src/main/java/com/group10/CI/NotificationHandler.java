78682ea6f5917499797e1a78827a2e41556b44c8
Ludwig Johansson <johanssons.ludwig@gmail.com>
2022-02-09 14:43:01 +0100

    Updated with support for Build class, fix #6

--- src/main/java/com/group10/CI/NotificationHandler.java
+++ src/main/java/com/group10/CI/NotificationHandler.java
@@ -12,16 +12,19 @@ import org.apache.http.impl.client.HttpClients;
 public class NotificationHandler {
 
     //Constructor creating a notilfication
-    public NotificationHandler(String status, String SHA){
-
+    public NotificationHandler(Build build){
+        notifyGitHub(build);
     }
 
     /**
      * Sending notifications on status to GitHub using the post API
      * @param status
      */
-    public void notifyGitHub(String status, String SHA){
-        String postURL = "https://api.github.com/repos/SE-Gorup10-CI/statuses/" + SHA;
+    public void notifyGitHub(Build build){
+        String repo = build.getRepo(); // Ex. ludwigjo/SE-Gorup10-CI
+        String prId = build.getPrId();
+        String status = build.getStatus(); //Not sure if we only should send the general status.
+        String postURL = "https://api.github.com/repos/" + repo  + "/" + prId;
         String postBody = "{\"state\":\"" + status + "\"}";
 
         //Sending the post request
