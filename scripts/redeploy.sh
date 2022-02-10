b5feba82f76a8a566de672a06c7dbfc955ad4bbe
Sefik-Palazoglu <44020012+Sefik-Palazoglu@users.noreply.github.com>
2022-02-10 14:23:11 +0100

    Add Scripts for Deployment of the App on Remote Linux Server (#24)
    * Add deploy scripts; feat #23
    
    * Add redeploy scripts, feat #23

---
+++ scripts/redeploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#redeploy the app on port $1
+
+if [ $# -eq 1 ]; then
+        PORT_NUMBER=$1
+
+        # Kill the working app,
+        SERVER_PID=$(pgrep -f Dexec.args="$1")
+        kill $SERVER_PID
+
+        # compile the app from current branch
+        mvn compile
+
+        # deploy app to the port $1
+        nohup mvn exec:java -D"exec.mainClass"="com.group10.CI.ContinuousIntegrationServer" -Dexec.args="$1" &> /dev/null &
+
+else
+        echo "Argument missing: You need to provide port to runt ngrok server"
+fi
