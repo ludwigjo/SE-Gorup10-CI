c465d9ba34b3068660e1a1c8dbeef0b8468efdeb
darkways <51851705+darkway-s@users.noreply.github.com>
2022-02-10 20:48:15 +0800

    add a negative unit test to TestHandler
    add a failure test.
    The process should return with the status of failure.

--- src/test/java/com/group10/CI/TestHandlerTest.java
+++ src/test/java/com/group10/CI/TestHandlerTest.java
@@ -49,4 +49,34 @@ public class TestHandlerTest {
         // tear down
         gitHandler.deleteClonedRepo(new File("temp"));
     }
+
+    /**
+     * Test the branch identify the test failure, which contains unpassed unit tests, thus
+     * the build should not be successful.
+     *
+     * It uses GitHandler to clone this project which is stored in a temp directory.
+     * The directory is removed on tear down.
+     *
+     * Asserts that status is set to FAILURE,
+     * and that the compilation information contains BUILD FAILURE (always outputted
+     * when using mvn compile and the build is unsuccessful).
+     */
+    @Test
+    @DisplayName("Test failure")
+    public void testCompileFailure() throws Exception {
+        // set up
+        String mockCommitHash = "1d18f8c74a81a5f376e2ab75fa7feb4cc539c9ea";
+        GitHandler gitHandler = new GitHandler("https://github.com/ludwigjo/SE-Gorup10-CI", "test/build-fail");
+        TestHandler ch = new TestHandler(mockCommitHash, gitHandler.getRepoPath());
+
+        // action
+        ch.test();
+
+        // assert
+        assertEquals(true, ch.getCompilationInformation().contains("BUILD FAILURE"));
+        assertEquals(Status.FAILURE, ch.getStatus(), "Status expected to be FAILURE if unsuccessful test.");
+
+        // tear down
+        gitHandler.deleteClonedRepo(new File("temp"));
+    }
 }
