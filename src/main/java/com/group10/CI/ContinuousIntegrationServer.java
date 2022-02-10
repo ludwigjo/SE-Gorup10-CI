60ddf9b421159cd6a19c765ada322ecf48dd575e
Amanda Krohn <akrohn@kth.se>
2022-02-10 21:59:41 +0100

    CIS clones, compiles and tests on POST request

--- src/main/java/com/group10/CI/ContinuousIntegrationServer.java
+++ src/main/java/com/group10/CI/ContinuousIntegrationServer.java
@@ -3,12 +3,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
+import netscape.javascript.JSObject;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import org.json.*;
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
@@ -27,12 +32,61 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         System.out.println(target);
 
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
+        // if server receives a webhook
+        if (request.getMethod().equals("POST")) {
+            handlePostRequest(request);
+        }
+
 
         response.getWriter().println("CI job done");
+    }
+
+    private void handlePostRequest(HttpServletRequest request) throws IOException {
+        JSONObject body = getBody(request);
+        if(body.equals(null)) return;
+        System.out.println("JSON BODY:\n" + body);
+
+        String[] ref = body.getString("ref").split("/");
+        // since split is on / we want the last two parts
+        String branch = String.join("", Arrays.copyOfRange(ref, (ref.length - 2), (ref.length - 1)));
+        String repoUrl = body.getJSONObject("repository").getString("url");
+        String commitSha = body.getString("after");
+        // clone
+        System.out.println("Cloning branch: " + branch + " from url: " + repoUrl);
+        GitHandler git = new GitHandler();
+        boolean hasCloned = git.cloneRepo(repoUrl, branch);
+        if(!hasCloned) return;
+
+        File f = new File(git.getRepoPath());
+        // compile
+        System.out.println("Compiling project.");
+        CompileHandler compileHandler = new CompileHandler(commitSha, repoUrl);
+        compileHandler.compile();
+        if(compileHandler.getStatus() == Status.ERROR) return;
+
+        // test
+        System.out.println("Testing project.");
+        TestHandler testHandler = new TestHandler(commitSha, repoUrl);
+        testHandler.test();
+        if(testHandler.getStatus() == Status.ERROR) return;
+
+        System.out.println("Build and testing completed.");
+    }
+
+    private JSONObject getBody(HttpServletRequest request){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            BufferedReader br = request.getReader();
+            String line;
+            while ((line = br.readLine()) != null) stringBuilder.append(line);
+            return new JSONObject(stringBuilder);
+        } catch (IOException e) {
+            System.out.println("ERROR");
+            System.out.println("Unable to get body from request: " + e.getMessage());
+            System.out.println("Returning...");
+            return null;
+        }
     }
 
     // used to start the CI server in command line
