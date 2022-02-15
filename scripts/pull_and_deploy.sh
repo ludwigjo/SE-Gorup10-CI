4fc5188cf5be7f2cbb6361f9a434e4ac9b58f70f
Sefik-Palazoglu <44020012+Sefik-Palazoglu@users.noreply.github.com>
2022-02-15 22:53:04 +0100

    Add log file functionality (#53)
    Fix #46.
    Now server logs the standard output and standard error output to log.log file.

--- scripts/pull_and_deploy.sh
+++ scripts/pull_and_deploy.sh
@@ -10,7 +10,7 @@ if [ $# -eq 1 ]; then
         #build project on this branch
         mvn compile
         #deploy to port $1
-        nohup mvn exec:java -D"exec.mainClass"="com.group10.CI.ContinuousIntegrationServer" -Dexec.args="$1" &> /dev/null &
+        nohup mvn exec:java -D"exec.mainClass"="com.group10.CI.ContinuousIntegrationServer" -Dexec.args="$1" &>> log.log &
 
 else
         echo "Argument missing: You need to provide port to runt ngrok server"
