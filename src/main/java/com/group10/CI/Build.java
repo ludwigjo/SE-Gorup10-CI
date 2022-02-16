4ec054aa92743bfa91b946a54a6f82eee43c1252
Bror Sebastian Sjövald <49447840+BrorSebastianSjovald@users.noreply.github.com>
2022-02-16 11:16:02 +0100

    Implemented saving of Build info, feat #60 (#64)
    * Added info fields to Build class, feat #60
    
    Added `buildInfo` and `testInfo` for saving command output.
    
    Reflected changes onto toString method.
    
    * Saved Build info when POSTing, feat #60
    
    When a POST request comes in it now saves the info in the Build object
    created.

--- src/main/java/com/group10/CI/Build.java
+++ src/main/java/com/group10/CI/Build.java
@@ -4,19 +4,22 @@ package com.group10.CI;
  * Build class taking care of the build information.
  */
 public class Build {
-    
+
     private String prId;
     private String email;
     private Status buildStatus;
+    private String buildInfo;
     private Status testStatus;
-    private String repo;    //The repo name. Ex. ludwigjo/SE-Group10-CI
+    private String testInfo;
+    private String repo; // The repo name. Ex. ludwigjo/SE-Group10-CI
 
     /**
      * Constructor for Build class.
-     * @param prId the prId of the build. Also called SHA in the gitHub API.
-     * @param email the email of the user who initiated the build.
+     * 
+     * @param prId        the prId of the build. Also called SHA in the gitHub API.
+     * @param email       the email of the user who initiated the build.
      * @param buildStatus the status of the build.
-     * @param testStatus the status of the tests.
+     * @param testStatus  the status of the tests.
      */
     public Build(String prId, String email, Status buildStatus, Status testStatus, String repo) {
         this.prId = prId;
@@ -25,11 +28,14 @@ public class Build {
         this.testStatus = testStatus;
         this.repo = repo;
     }
-    //Default constructor
-    public Build() {}
+
+    // Default constructor
+    public Build() {
+    }
 
     /**
      * Get method for prId.
+     * 
      * @return prId the id for a pull request. Also called SHA in the gitHub API.
      */
     public String getPrId() {
@@ -38,6 +44,7 @@ public class Build {
 
     /**
      * Get method for email.
+     * 
      * @return email the email of the user.
      */
     public String getEmail() {
@@ -46,6 +53,7 @@ public class Build {
 
     /**
      * Get method for buildStatus.
+     * 
      * @return buildStatus the status of the build.
      */
     public Status getBuildStatus() {
@@ -53,7 +61,17 @@ public class Build {
     }
 
     /**
+     * Get method for buildInfo.
+     * 
+     * @return buildInfo the standard output of the build process.
+     */
+    public String getBuildInfo() {
+        return buildInfo;
+    }
+
+    /**
      * Get method for testStatus.
+     * 
      * @return testStatus the status of the tests.
      */
     public Status getTestStatus() {
@@ -61,7 +79,17 @@ public class Build {
     }
 
     /**
+     * Get method for testInfo.
+     * 
+     * @return testInfo the standard output for test process.
+     */
+    public String getTestInfo() {
+        return testInfo;
+    }
+
+    /**
      * Get method for repo.
+     * 
      * @return repo the repo name. Ex. ludwigjo/SE-Group10-CI
      */
     public String getRepo() {
@@ -70,12 +98,16 @@ public class Build {
 
     /**
      * Get method for the build
-     * @return  this build object
-     * */
-    public Build getBuild(){ return this; }
+     * 
+     * @return this build object
+     */
+    public Build getBuild() {
+        return this;
+    }
 
     /**
      * Set method for prId.
+     * 
      * @param prId the id for a pull request. Also called SHA in the gitHub API.
      */
     public void setPrId(String prId) {
@@ -84,6 +116,7 @@ public class Build {
 
     /**
      * Set method for email.
+     * 
      * @param email the email of the user.
      */
     public void setEmail(String email) {
@@ -92,6 +125,7 @@ public class Build {
 
     /**
      * Set method for buildStatus.
+     * 
      * @param buildStatus the status of the build.
      */
     public void setBuildStatus(Status buildStatus) {
@@ -99,7 +133,17 @@ public class Build {
     }
 
     /**
+     * Set method for buildInfo.
+     * 
+     * @param buildInfo the standard output of the build process.
+     */
+    public void setBuildInfo(String buildInfo) {
+        this.buildInfo = buildInfo;
+    }
+
+    /**
      * Set method for testStatus.
+     * 
      * @param testStatus the status of the tests.
      */
     public void setTestStatus(Status testStatus) {
@@ -107,18 +151,30 @@ public class Build {
     }
 
     /**
+     * Set method for testInfo.
+     * 
+     * @param testInfo the standard output of the test process.
+     */
+    public void setTestInfo(String testInfo) {
+        this.testInfo = testInfo;
+    }
+
+    /**
      * Set method for repo.
+     * 
      * @param repo the repo name. Ex. ludwigjo/SE-Group10-CI
      */
     public void setRepo(String repo) {
         this.repo = repo;
     }
 
-    public String toString(){
+    public String toString() {
         String s = "";
         s += "Commit SHA: " + this.getPrId();
         s += "\nBuild Status: " + this.getBuildStatus();
-        s += "\nTest Status" + this.getTestStatus();
+        s += "\nBuild info: " + this.getBuildInfo();
+        s += "\nTest Status: " + this.getTestStatus();
+        s += "\nTest info: " + this.getTestInfo();
         return s;
     }
 }
